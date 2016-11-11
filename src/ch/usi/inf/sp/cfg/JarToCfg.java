2bee420b08f0b0acbb1cad3ebd5f3b64e95af838
EricBotter <ericbotter@usi.ch>
2016-11-11 17:14:08 +0100

    Added method in CFGExtractor Added JarToCfg class to parse JAR files

---
+++ src/ch/usi/inf/sp/cfg/JarToCfg.java
@@ -0,0 +1,27 @@
+package ch.usi.inf.sp.cfg;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.objectweb.asm.ClassReader;
+
+public class JarToCfg {
+    public static void main(final String[] args) throws IOException {
+        final String jarFileName = args[0];
+        System.out.println("Analyzing "+jarFileName);
+        final JarFile jar = new JarFile(jarFileName);
+        final Enumeration<JarEntry> entries = jar.entries();
+        while (entries.hasMoreElements()) {
+            final JarEntry entry = entries.nextElement();
+            if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                System.out.println(entry.getName());
+                final InputStream is = jar.getInputStream(entry);
+                ControlFlowGraphExtractor.analyzeClassReader(new ClassReader(is));
+            }
+        }
+    }
+
+}
